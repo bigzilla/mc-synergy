22a0ce85d3fbff38102e0179005e7376105a28cf
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2020-05-22 02:12:47 +0700

    feat: change favicon and title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   publicPath: process.env.NODE_ENV === "production" ? "/mc-synergy/" : "/",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+      title: "Magic Chess Synergy Builder",
+    },
+  },
 };
