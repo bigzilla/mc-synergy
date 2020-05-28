9ef24a781d23cd70d508ad0f2b32d0f89669d395
Billy Zaelani Malik <m.billyzaelani@gmail.com>
2020-05-29 02:39:59 +0700

    feat: add AppBar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,40 @@
 <template>
   <v-app>
-    <v-container fluid class="fill-height">
-      <v-row align-content="space-between" class="fill-height">
-        <!-- row 1 -->
-        <v-col cols="2">
-          <TriggeredSynergies :deck="deck" />
-        </v-col>
-        <v-col cols="8">
-          <HeroPool @select-hero="selectHero" />
-        </v-col>
-        <v-col cols="2">
-          <v-card>
-            <!-- reserved for additional information -->
-          </v-card>
-        </v-col>
+    <AppBar />
+    <v-content>
+      <v-container fluid class="fill-height py-0">
+        <v-row align-content="space-between" class="fill-height">
+          <!-- row 1 -->
+          <v-col cols="2">
+            <TriggeredSynergies :deck="deck" />
+          </v-col>
+          <v-col cols="8" class="pb-0">
+            <HeroPool @select-hero="selectHero" />
+          </v-col>
+          <v-col cols="2">
+            <v-card>
+              <!-- reserved for additional information -->
+            </v-card>
+          </v-col>
 
-        <!-- row 2 -->
-        <v-col cols="1">
-          <CommanderLevel :heroSlot="deck.length" @add-slot="addSlot" v-model="scienceCrystal" />
-        </v-col>
-        <v-col cols="10">
-          <HeroDeck :deck="deck" v-model="slotIndex" @delete-hero="deleteHero" />
-        </v-col>
-        <v-col cols="1">
-          <ResetButton @reset="reset" />
-        </v-col>
-      </v-row>
-    </v-container>
+          <!-- row 2 -->
+          <v-col cols="1">
+            <CommanderLevel :heroSlot="deck.length" @add-slot="addSlot" v-model="scienceCrystal" />
+          </v-col>
+          <v-col cols="10">
+            <HeroDeck :deck="deck" v-model="slotIndex" @delete-hero="deleteHero" />
+          </v-col>
+          <v-col cols="1">
+            <ResetButton @reset="reset" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
+import AppBar from "./components/AppBar";
 import TriggeredSynergies from "./components/TriggeredSynergies";
 import HeroPool from "./components/HeroPool";
 import CommanderLevel from "./components/CommanderLevel";
@@ -42,6 +46,7 @@ import heroes from "./assets/js/heroes";
 export default {
   name: "App",
   components: {
+    AppBar,
     TriggeredSynergies,
     HeroPool,
     CommanderLevel,
---
+++ src/components/AppBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar color="grey" dense app>
+    <v-spacer></v-spacer>
+    <v-tooltip bottom v-for="(social, i) in socials" :key="i">
+      <template v-slot:activator="{on}">
+        <v-btn icon v-on="on" target="_blank" :href="social.link">
+          <v-icon>{{social.icon}}</v-icon>
+        </v-btn>
+      </template>
+      <span>{{social.tooltip}}</span>
+    </v-tooltip>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data: () => ({
+    socials: [
+      {
+        link: "https://youtube.com/channel/UCruYoRD0VdY9Mruf9ox6JrQ",
+        icon: "mdi-youtube",
+        tooltip: "View me on Youtube"
+      },
+      {
+        link: "https://instagram.com/m.billyzaelani",
+        icon: "mdi-instagram",
+        tooltip: "View me on Instagram"
+      },
+      {
+        link: "https://github.com/billyzaelani/mc-synergy",
+        icon: "mdi-github",
+        tooltip: "View me on Github"
+      }
+    ]
+  })
+};
+</script>
